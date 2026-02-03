e4f2606767229c1dc02d50f174c7bf875ce87cae
Nirmalk2006 <nirmalkharche2006@gmail.com>
2026-02-03 00:17:21 -0800

    commit

---
+++ 1variables.js
@@ -0,0 +1,15 @@
+let name="hitesh"      //name is variable
+let age=20;            // age is variable
+const pi=3.14;         // pi is constant variable
+console.log(name);
+console.log(name);
+
+//var keyword allows redeclaration in scope
+var count = 1;
+var count = 2; // no error
+console.log(count);
+
+// let keyword does not allow redeclaration in the same scope
+let total = 1;
+let total = 2; // SyntaxError
+console.log(total);
